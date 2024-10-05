0c88a9d05004b31462179c3f30bdb8e3cc3fcf11
Razavik <104317397+Razavik@users.noreply.github.com>
2024-10-06 00:15:26 +0300

    Delete TypeScript directory

--- TypeScript/lab3/7_generic.js
+++
@@ -1,7 +0,0 @@
-var arrayOfNumbers = [1, 1, 2, 3, 5];
-var arrayOfStrings = ['Hello', 'Vladilen'];
-function reverse(array) {
-    return array.reverse();
-}
-reverse(arrayOfNumbers);
-reverse(arrayOfStrings);
--- TypeScript/lab3/8_operators.js
+++
@@ -1,4 +0,0 @@
-var key = 'name';
-key = 'age';
-var u1 = 'name';
-// u1 = '_id'
